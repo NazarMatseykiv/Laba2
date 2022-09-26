2b495f4587a57f2c3dab3cd9ccee5509b975834f
nazar <nazar@DESKTOP-A6PNJH5>
2022-09-26 10:57:50 +0300

    Добавьте файлы проекта.

---
+++ ProgramEx1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first array: ");
+            string[] arr = Console.ReadLine().Split(' ');
+            Console.WriteLine("Enter second array: ");
+            string[] arr2 = Console.ReadLine().Split(' ');
+            int min = Math.Min(arr.Length, arr2.Length);
+            int left = 0;
+            int right = 0;
+            for(int i = 0; i < min; i++)
+            {
+                if(arr[i] != arr2[i])
+                {
+                    break;
+                }
+                left++;
+            }
+            arr = arr.Reverse().ToArray();
+            arr2 = arr2.Reverse().ToArray();
+            for (int i = 0; i < min; i++)
+            {
+                if (arr[i] != arr2[i])
+                {
+                    break;
+                }
+                right++;
+            }
+            Console.WriteLine(Math.Max(left, right));
+        }
+    }
+}
